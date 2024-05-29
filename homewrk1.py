4f068360f6ff199d9787dd66217b72c9f1272ff6
Davlatov Rustam <davlatov_rm@mail.ru>
2024-05-30 00:40:54 +0300

    Add files via upload
    Практическое задание по уроку "Базовые структуры данных

---
+++ homewrk1.py
@@ -0,0 +1,23 @@
+a = 'It is raining now'
+print(len (a))
+first = 123
+second = 218
+summa = first + second
+print(summa)
+diff = second - first
+print(diff)
+first = 213
+second = 411
+third = 399
+main = (first + second + third) / 3
+print(main)
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(second_string + ',',first_string)
+a = 7
+b = 6
+c = 4
+f = (a * b) + (a * c)
+print(f)
+y = 3 #степень
+print(f**y / 2)
