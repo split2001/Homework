c10dd8910c996845470e7c81f3097bfa37ed65b8
Davlatov Rustam <davlatov_rm@mail.ru>
2024-05-31 02:41:52 +0300

    Add files via upload
    Практическое задание по теме "Переменные

---
+++ homewrk3.py
@@ -0,0 +1,5 @@
+task = 12
+time = 1.5
+name = 'Python'
+result = (time / task)
+print('Курс:' + name,',','всего задач:',task,',','затрачено часов:',time,',','среднее время выполнения ',result,'.', sep = '')
