baec7ccd0571b00154e30a69a3f3909f22d3c1db
Davlatov Rustam <davlatov_rm@mail.ru>
2024-06-01 01:02:38 +0300

    Add files via upload
    Практическая работа по уроку "Динамическая типизация"

---
+++ homework4.py
@@ -0,0 +1,8 @@
+name = "Rustam"
+print('Name:'+ name)
+age = 28
+print('Age:' + (str(age)))
+age = age + 1
+print('New age:' + (str(age)))
+is_student = True
+print('Is student:'+ (str(is_student)))
