93a2fd5f6db342b0dc51451f0b5516d90492b735
Davlatov Rustam <davlatov_rm@mail.ru>
2024-08-24 12:22:19 +0300

    Add files via upload
    Домашнее задание по теме "Генераторы"

---
+++ homework6m9.py
@@ -0,0 +1,11 @@
+from itertools import product
+def all_variants(text):
+    for i in range(len(text)):
+        for j in range(i, len(text)):
+            yield text[i:j + 1]
+
+a = all_variants("abc")
+for i in a:
+    print(i)
+
+
