a41a82094320c45e5a694fb8b993af3645598df1
Davlatov Rustam <davlatov_rm@mail.ru>
2024-08-25 14:49:28 +0300

    Add files via upload
    Домашнее задание по теме "Многопроцессное программирование"

---
+++ homework5m10 multiprocessing.py
@@ -0,0 +1,30 @@
+import multiprocessing
+import time
+from datetime import datetime
+
+def read_info(name):
+    with open(name, 'r', encoding='utf-8') as f:
+        all_data = f.readlines()
+
+time_start = datetime.now()
+filenames = [f'./file {number}.txt' for number in range(1, 5)]
+for file in filenames:
+    read_info(file)
+time_end = datetime.now()
+time_res = time_end - time_start
+print(time_res)
+
+
+
+if __name__ == '__main__':
+    filenames = [f'./file {number}.txt' for number in range(1, 5)]
+    for file in filenames:
+        with multiprocessing.Pool(processes=4) as pool:
+            time_start = datetime.now()
+            pool.map(read_info, filenames)
+    time_end = datetime.now()
+    time_res = time_end - time_start
+    print(time_res)
+
+
+
