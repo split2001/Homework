2e7e9d3ee03d8da91a322f9a45958236d4bf304b
Davlatov Rustam <davlatov_rm@mail.ru>
2024-09-18 00:49:27 +0300

    Add files via upload
    Домашнее задание по теме "Методы Юнит-тестирования"

---
+++ test_runner2.py
@@ -0,0 +1,41 @@
+import unittest
+from runner2 import *
+
+
+class TournamentTest(unittest.TestCase):
+
+    def setUp(self):
+        self.runner = Runner('Усэйн', 10)
+        self.runner2 = Runner('Андрей', 9)
+        self.runner3 = Runner('Ник', 3)
+
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
+
+    def test_start1(self):
+        tour = Tournament(90, self.runner, self.runner3)
+        result = tour.start()
+        self.all_results['Тест1'] = result
+        self.assertTrue(max(self.all_results.keys()), self.runner3)
+
+    def test_start2(self):
+        tour2 = Tournament(90, self.runner2, self.runner3)
+        result = tour2.start()
+        self.all_results['Тест2'] = result
+        self.assertTrue(max(self.all_results.keys()), self.runner3)
+
+    def test_start3(self):
+        tour3 = Tournament(90, self.runner, self.runner2, self.runner3)
+        result = tour3.start()
+        self.all_results['Тест3'] = result
+        self.assertTrue(max(self.all_results.keys()), self.runner3)
+
+    @classmethod
+    def tearDownClass(cls):
+        for key, value in cls.all_results.items():
+            print(f'{key}:{value}\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
