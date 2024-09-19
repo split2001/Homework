aaedfac19a4a6fe0aa76e832f46eb21303d7f4ab
Davlatov Rustam <davlatov_rm@mail.ru>
2024-09-19 18:44:04 +0300

    Update test_runner2.py

--- test_runner2.py
+++ test_runner2.py
@@ -3,6 +3,7 @@ from runner2 import *
 
 
 class TournamentTest(unittest.TestCase):
+    is_frozen = True
 
     def setUp(self):
         self.runner = Runner('Усэйн', 10)
@@ -13,18 +14,21 @@ class TournamentTest(unittest.TestCase):
     def setUpClass(cls):
         cls.all_results = {}
 
+    @unittest.skipIf(is_frozen == True, 'Тесты в этом кейсе заморожены')
     def test_start1(self):
         tour = Tournament(90, self.runner, self.runner3)
         result = tour.start()
         self.all_results['Тест1'] = result
         self.assertTrue(max(self.all_results.keys()), self.runner3)
 
+    @unittest.skipIf(is_frozen == True,'Тесты в этом кейсе заморожены')
     def test_start2(self):
         tour2 = Tournament(90, self.runner2, self.runner3)
         result = tour2.start()
         self.all_results['Тест2'] = result
         self.assertTrue(max(self.all_results.keys()), self.runner3)
 
+    @unittest.skipIf(is_frozen == True,'Тесты в этом кейсе заморожены')
     def test_start3(self):
         tour3 = Tournament(90, self.runner, self.runner2, self.runner3)
         result = tour3.start()
