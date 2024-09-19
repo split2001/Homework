8bd8b72cbd720dec52500529131b44f2087b497d
Davlatov Rustam <davlatov_rm@mail.ru>
2024-09-19 18:44:40 +0300

    Update test2.py

--- test2.py
+++ test2.py
@@ -3,21 +3,25 @@ from runner import *
 
 
 class RunnerTest(unittest.TestCase):
+    is_frozen = False
 
     def setUp(self):
         self.walker = Runner('bob')
         self.runner = Runner('not_bob')
 
+    @unittest.skipIf(is_frozen == True, 'Тесты в этом кейсе заморожены')
     def test_walk(self):
         for i in range(10):
             self.walker.walk()
         self.assertEqual(self.walker.distance, 50)
 
+    @unittest.skipIf(is_frozen == True, 'Тесты в этом кейсе заморожены')
     def test_run(self):
         for i in range(10):
             self.runner.run()
         self.assertEqual(self.runner.distance, 100)
 
+    @unittest.skipIf(is_frozen == True, 'Тесты в этом кейсе заморожены')
     def test_challenge(self):
         for i in range(10):
             self.walker.walk()
